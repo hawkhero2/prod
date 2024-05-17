5ed236c304c214790d16b82180ff20382939faf9
Ivanciu Andrei <47902631+hawkhero2@users.noreply.github.com>
2024-05-17 16:36:48 +0300

    refactor, added todo, added links
    Small refactor. Wrote TODO containing a list with the apps that need to be scrapped. Created class for scrapping the websites for the infomation

--- src/main/java/j/software/versions/App.java
+++ src/main/java/j/software/versions/App.java
@@ -13,34 +13,48 @@ Windows Defender Security Center,Sonicwall NetExtender,PGina
 FortiClient, Azure VPN Client, Libre Office
 */
  
+ 
 {
     public static void main( String[] args ) {
-        String[] urls = {"https://versionhistory.googleapis.com/v1/chrome/platforms/win64/channels/canary/versions/all/releases?filter=endtime=none",
+        String[] urls = {"https://helpx.adobe.com/acrobat/release-note/release-notes-acrobat-reader.html",
+        "https://www.cisco.com/c/en/us/support/security/anyconnect-secure-mobility-client/products-release-notes-list.html",
+        "https://www.irfanview.com/main_history.htm",
+        "https://www.videolan.org/vlc/releases/"};
+
+        String[] apiUrls = {"https://versionhistory.googleapis.com/v1/chrome/platforms/win64/channels/canary/versions/all/releases?filter=endtime=none",
         "https://product-details.mozilla.org/1.0/firefox_versions.json"};
 
-        for (String url : urls) {
+        for (String url : apiUrls) {
             try {
                 ApiHandler apiHandler = new ApiHandler();
                 String responseStr = apiHandler.get(url);
 
-                ArrayList<String> versions = new ArrayList<>(urls.length);
+                ArrayList<String> versions = new ArrayList<>(apiUrls.length);
 
                 for ( String item : responseStr.split(",")) {
                     
-                    if ( url.contains("chrome")) {
+                    if (url.contains("chrome")) {
 
-                        if( item.contains("\"version\"") ) {
+                        if(item.contains("\"version\"") ) {
                             versions.add("chrome:"+item.split(":")[1]+",");
                         }
                     }
 
-                    if ( url.contains("mozilla")) {
+                    if (url.contains("mozilla")) {
                         if (item.contains("\"LATEST_FIREFOX_VERSION\"")) {
                             versions.add(item+",");
                         }
                     }
                 }
             System.out.println(versions.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (String url : urls) {
+            try {
+                
             } catch (Exception e) {
                 e.printStackTrace();
             }
