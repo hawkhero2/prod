feb1f40db4b56151d549c186817b231393faabf3
Ivanciu Andrei <47902631+hawkhero2@users.noreply.github.com>
2024-10-23 00:34:22 +0300

    refactor get() funct by splitting ifs into funcs

--- src/main/java/j/software/versions/ScrappingHandler.java
+++ src/main/java/j/software/versions/ScrappingHandler.java
@@ -11,9 +11,92 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class ScrappingHandler {
+    String url;
     
     public ScrappingHandler() {
+    }
+
+    public ScrappingHandler(String url) {
+       this.url = url; 
+    }
+
+    public String irfanviewParser(Document doc) {
+        String responseString="";
         
+        Element versionElement = doc.selectFirst("h3");
+        if (versionElement != null ) {
+            return responseString = "IrfanView: "+versionElement.text();
+        }
+        
+       return responseString;
+    }
+
+    public String skypeParser( Document doc) {
+        String responseString="";
+        Elements elements = doc.select("p");
+        for (Element item : elements) {
+            if(item.text().contains("Skype for Windows Desktop version")){
+            return responseString = "Skype: "+item.text();
+            }
+        }
+        return responseString;
+    }
+
+    public String vlcParser(Document doc) {
+        String responseString="";
+
+        // VLC
+        Elements elements = doc.select("a");
+        for (Element item : elements) {
+            if ((item.text() != null) && (item.text().contains("VLC")) && (item.attr("href").contains("/vlc/releases/")) ) {
+                return responseString = "VLC: "+item.text();
+            } 
+        }
+        return responseString;
+    }
+
+    public String adobeParser(Document doc) {
+        String responseString="";
+
+        Element versionElement = doc.selectFirst("span[class='std std-ref']");
+        return responseString = "Adobe Acrobat: "+versionElement.text();
+    }
+
+    public String ciscoParser(Document doc) {
+        String responseString="";
+
+        Elements elements = doc.select("a[data-id='link4']");
+        if (elements.size() > 0) {
+            for (Element item : elements) {
+                if (item.text().contains("AnyConnect Secure Mobility Client")) {
+                    String splitted = item.text().split(",")[1];
+                    return responseString = "Cisco: "+splitted;
+                }
+            }
+        }
+
+        return responseString;
+    }
+
+    public String fortiParser(Document doc) {
+        String responseString="";
+        Element element = doc.selectFirst("h2[class='new--design']");
+        if ((element != null) && (element.text().contains("FortiClient"))) {
+            return responseString = "FortiClient: "+element.text().trim().split(" ")[1];
+        }
+
+        return responseString;
+    }
+
+    public String libreOfficeParser(Document doc) {
+        String responseString="";
+        Elements elements = doc.select("h4");
+        for(Element item : elements) {
+            if( (item.text() != null) && (item.text().contains("Latest Release")) ) {
+                return responseString = "LibreOffice: "+item.text();
+            }
+        }
+        return responseString;
     }
 
     public String get(String url) {
@@ -36,83 +119,10 @@ public class ScrappingHandler {
             String htmlStr = strBuilder.toString();
             Document doc = Jsoup.parse(htmlStr);
             
-            // Adobe Acrobat
-            if (url.contains("adobe")) {
-                Element versionElement = doc.selectFirst("span[class='std std-ref']");
-                return responseString = "Adobe Acrobat: "+versionElement.text();
+            if (url.contains("forti")) {
+                responseString = fortiParser(doc);
             }
-
-            // Cisco
-            if (url.contains("cisco")) {
-               Elements elements = doc.select("a[data-id='link4']");
-                if ( elements.size() > 0) {
-                    for ( Element item : elements) {
-                        if ( item.text().contains("AnyConnect Secure Mobility Client")) {
-                            String splitted = item.text().split(",")[1];
-                            return responseString = "Cisco: "+splitted;
-                        }
-                    }
-                }
-            }
-
-            // irfanview
-            if (url.contains("irfanview")) {
-                Element versionElement = doc.selectFirst("h3");
-                if ( versionElement != null ) {
-                    return responseString = "IrfanView: "+versionElement.text();
-                }
-            }
-
-            // VLC
-            if ( url.contains("videolan")) {
-                Elements elements = doc.select("a");
-                for ( Element item : elements) {
-                    if ( (item.text() != null) && (item.text().contains("VLC")) && (item.attr("href").contains("/vlc/releases/")) ) {
-                        // System.out.println("VLC version found: "+item.text());
-                        return responseString = "VLC: "+item.text();
-                    } 
-                }
-            }
-
-            // Skype
-            if ( url.contains("skype")) {
-                Elements elements = doc.select("p");
-
-                for (Element item : elements) {
-
-                    if( item.text().contains("Skype for Windows Desktop version")){
-                    return responseString = "Skype: "+item.text();
-                    }
-                }
-            } 
-
-            // Forticlient
-            if(url.contains("fortinet")) {
-                Element element = doc.selectFirst("h2[class='new--design']");
-                if ((element != null) && (element.text().contains("FortiClient"))) {
-                    return responseString = "FortiClient: "+element.text().trim().split(" ")[1];
-                }
-                
-                // for (Element item : elements) {
-                //     if ((item.text() != null) && (item.text().contains("FortiClient")) ) {
-                //         // System.out.println("FortiClient version found: "+item.text());
-                //         return responseString = "FortiClient: "+item.text();
-                //     } 
-                // }
-                
-            }
-
-            // LibreOffice
-            if(url.contains("libreoffice")) {
-                Elements elements = doc.select("h4");
-                for(Element item : elements) {
-                    if( (item.text() != null) && (item.text().contains("Latest Release")) ) {
-                        // System.out.println("LibreOffice version found: "+item.text());
-                        return responseString = "LibreOffice: "+item.text();
-                    }
-                }
-            }
-
+            
         } catch (Exception e ) {
             e.printStackTrace();
         }
